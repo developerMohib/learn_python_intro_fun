1878e5f4e4a4f62dfb230567aefbd4406370e606
developerMohib <mohibullahmohim2020@gmail.com>
2024-12-29 20:30:20 +0600

    first explore in youtube about python

---
+++ into.py
@@ -0,0 +1,13 @@
+name = "khan saheb"
+print(name)
+num1 = 44
+num2 = 22
+
+print(f'joogfol {num1+num2} ')
+
+name = "Mohib"
+age = 24
+height = 5.4
+
+# Using f-string
+print(f"My name is {name}, I am {age} years old, and my height is {height} feet.")
