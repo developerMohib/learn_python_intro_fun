54656c0577f3d4961d4413f6f84422dbcb6018f1
developerMohib <mohibullahmohim2020@gmail.com>
2025-05-21 22:02:53 +0600

    Nezha Complains Count solve in codeforce and virtual judge

---
+++ nezhaComplainCount.py
@@ -0,0 +1,5 @@
+A, B, C, D = map(int, input().split())
+
+total_time = A + B + C
+complains = total_time * D
+print(complains)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# How to run
+-- See the right side of vs code a RUN BUTTON or PLAY BUTTON
+-- Click Here
